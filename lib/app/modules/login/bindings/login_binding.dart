470e114d5f0e7dc207e4804299748529178f03ea
notsatria <buanasatriaa@gmail.com>
2024-02-06 14:02:06 +0700

    added login with google (not done yet)

--- lib/app/modules/login/bindings/login_binding.dart
+++ lib/app/modules/login/bindings/login_binding.dart
@@ -1,10 +1,15 @@
 import 'package:get/get.dart';
 
+import 'package:fisimate/app/modules/login/controllers/login_with_google_controller.dart';
+
 import '../controllers/login_controller.dart';
 
 class LoginBinding extends Bindings {
   @override
   void dependencies() {
+    Get.lazyPut<LoginWithGoogleController>(
+      () => LoginWithGoogleController(),
+    );
     Get.lazyPut<LoginController>(
       () => LoginController(),
     );
