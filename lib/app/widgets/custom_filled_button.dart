78e32dcb67d039aeea409020df8aee16bf74ece4
haidara <haidarhaidara9@gmail.com>
2024-02-06 10:23:24 +0700

    Nf splash UI (#10)
    * view:animation splashscreen
    
    * fix: animation smoother

--- lib/app/widgets/custom_filled_button.dart
+++ lib/app/widgets/custom_filled_button.dart
@@ -7,11 +7,15 @@ class CustomFilledButton extends StatelessWidget {
   final double? width;
   final double? height;
   final String text;
+  final Color? color;
+  final double? radius;
   final void Function()? onTap;
   const CustomFilledButton({
     super.key,
     this.width,
     this.height,
+    this.radius,
+    this.color = const Color(0xff072DF4),
     required this.onTap,
     required this.text,
   });
@@ -24,8 +28,8 @@ class CustomFilledButton extends StatelessWidget {
         width: width ?? CustomSize.maxWidth,
         height: height ?? CustomSize.maxHeight / 13.5,
         decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(75.0),
-          color: CustomColor.blueColor,
+          borderRadius: BorderRadius.circular(radius ?? 75.0),
+          color: color,
         ),
         child: Center(
           child: Text(
