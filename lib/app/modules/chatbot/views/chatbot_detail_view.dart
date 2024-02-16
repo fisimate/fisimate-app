5f6affaaad6af3784c765e56e9ce58242aca8db9
Rafi Adi Pramana <rafiadipramana@gmail.com>
2024-02-16 22:23:37 +0700

    debug(chatbot): fix chatbot ui

--- lib/app/modules/chatbot/views/chatbot_detail_view.dart
+++ lib/app/modules/chatbot/views/chatbot_detail_view.dart
@@ -1,5 +1,6 @@
 import 'package:fisimate/app/theme/colors.dart';
 import 'package:fisimate/app/theme/fonts.dart';
+import 'package:fisimate/app/theme/sizing.dart';
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 
@@ -22,6 +23,7 @@ class ChatbotDetailView extends StatelessWidget {
                   borderRadius: BorderRadius.circular(30),
                 ),
                 child: TextField(
+                  style: subHeadingRegular,
                   decoration: InputDecoration(
                     hintText: 'Ketik pesan di sini...',
                     hintStyle: subHeadingRegular,
@@ -76,60 +78,79 @@ class BubbleChat extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     if (isMe) {
-      return Container(
-        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
-        margin: const EdgeInsets.only(bottom: 10, top: 10),
-        decoration: BoxDecoration(
-          color: CustomColor.bankRumus,
-          borderRadius: const BorderRadius.only(
-            topLeft: Radius.circular(20),
-            topRight: Radius.circular(20),
-            bottomLeft: Radius.circular(20),
+      return Row(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+            margin: const EdgeInsets.only(bottom: 10, top: 10),
+            constraints: BoxConstraints(
+              maxWidth: CustomSize.maxWidth * 0.6,
+            ),
+            decoration: BoxDecoration(
+              color: CustomColor.bankRumus,
+              borderRadius: const BorderRadius.only(
+                topLeft: Radius.circular(20),
+                topRight: Radius.circular(20),
+                bottomLeft: Radius.circular(20),
+              ),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Text(
+                  'Apa itu Hukum Newton?',
+                  style: subHeadingRegular.copyWith(color: CustomColor.whiteColor),
+                ),
+                const Gap(5.0),
+                Text(
+                  '10:00',
+                  style: bodyMedium.copyWith(
+                      fontSize: 12, color: CustomColor.whiteColor),
+                ),
+              ],
+            ),
           ),
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            Text(
-              'Apa itu Hukum Newton?',
-              style: subHeadingRegular.copyWith(color: CustomColor.whiteColor),
-            ),
-            const Gap(5.0),
-            Text(
-              '10:00',
-              style: bodyMedium.copyWith(
-                  fontSize: 12, color: CustomColor.whiteColor),
-            ),
-          ],
-        ),
+        ],
       );
     } else {
-      return Container(
-        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
-        margin: const EdgeInsets.only(bottom: 10, top: 10),
-        decoration: BoxDecoration(
-          color: CustomColor.greyColor.withOpacity(0.3),
-          borderRadius: const BorderRadius.only(
-            topLeft: Radius.circular(20),
-            topRight: Radius.circular(20),
-            bottomRight: Radius.circular(20),
+      return Row(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+            margin: const EdgeInsets.only(bottom: 10, top: 10),
+            constraints: BoxConstraints(
+              maxWidth: CustomSize.maxWidth * 0.6,
+            ),
+            decoration: BoxDecoration(
+              color: CustomColor.greyColor.withOpacity(0.3),
+              borderRadius: const BorderRadius.only(
+                topLeft: Radius.circular(20),
+                topRight: Radius.circular(20),
+                bottomRight: Radius.circular(20),
+              ),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Text(
+                  'Hi, Nabila! Tanyakan apapun tentang fisika dengan My Fisimate',
+                  style: subHeadingRegular,
+                  overflow: TextOverflow.visible,
+                ),
+                const Gap(5.0),
+                Text(
+                  '10:00',
+                  style: bodyMedium.copyWith(
+                      fontSize: 12, color: CustomColor.darkGreyColor),
+                ),
+              ],
+            ),
           ),
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            Text(
-              'Hi, Nabila! Tanyakan apapun tentang fisika dengan My Fisimate',
-              style: subHeadingRegular,
-            ),
-            const Gap(5.0),
-            Text(
-              '10:00',
-              style: bodyMedium.copyWith(
-                  fontSize: 12, color: CustomColor.darkGreyColor),
-            ),
-          ],
-        ),
+        ],
       );
     }
   }
