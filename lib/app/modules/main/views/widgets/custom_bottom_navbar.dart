be8360282d3d694a1647d16a501c445c79dea191
Rafi Adi Pramana <88870768+rafiadipramana@users.noreply.github.com>
2024-05-25 02:03:55 +0700

    style(home): match widgets properties with ui design (#23)

--- lib/app/modules/main/views/widgets/custom_bottom_navbar.dart
+++ lib/app/modules/main/views/widgets/custom_bottom_navbar.dart
@@ -14,6 +14,7 @@ class CustomBottomNavigationBar extends StatelessWidget {
     return Container(
       padding: const EdgeInsets.symmetric(vertical: 10),
       width: double.infinity,
+      color: CustomColor.whiteColor,
       child: Row(
         children: <Widget>[
           _buildNavbarItem(
